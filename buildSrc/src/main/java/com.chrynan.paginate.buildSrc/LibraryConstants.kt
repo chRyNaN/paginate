257e8e3ec19c7c1d849b8052b10718b263c46b0f
Christopher Keenan <ckeenan@starry.com>
2022-04-19 16:46:41 -0500

    Bumped version to 0.3.0 (4)

--- buildSrc/src/main/java/com.chrynan.paginate.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.paginate.buildSrc/LibraryConstants.kt
@@ -2,20 +2,19 @@ package com.chrynan.paginate.buildSrc
 
 object LibraryConstants {
 
-    const val bintrayRepo = "chrynan"
     const val group = "com.chrynan.paginate"
     const val owner = "chrynan"
     const val repoName = "paginate"
-    const val versionName = "0.2.1"
-    const val versionCode = 3
-    const val versionDescription = "Pre-release 0.2.1"
+    const val versionName = "0.3.0"
+    const val versionCode = 4
+    const val versionDescription = "Pre-release 0.3.0"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/paginate.git"
 
     object Android {
 
-        const val compileSdkVersion = 30
+        const val compileSdkVersion = 31
         const val minSdkVersion = 23
-        const val targetSdkVersion = 29
+        const val targetSdkVersion = 31
     }
 }
